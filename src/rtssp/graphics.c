c312f968813ebdc60b42666026d88406c5a2fca8
jkstpierre <jkstpierre@wpi.edu>
2019-11-20 16:25:56 -0500

    Writing sphere construction code

--- src/rtssp/graphics.c
+++ src/rtssp/graphics.c
@@ -13,12 +13,37 @@
 // INCLUDES //
 
 #include "rtssp/graphics.h"
+#include <math.h>
 
 
 // FUNCTIONS //
 
 mesh_t buildSphereMesh(GLfloat radius, GLuint sectors, GLuint stacks) {
+  // Fields
+  mesh_t sphere;  // The mesh we are going to return
+  vertex_t *vertex_array = NULL;  // The vertex array to build
+
+  // Compute the number of vertices to appear in the sphere
+  sphere.vertex_count = (stacks + 1) * (sectors + 1);
   
+  // Allocate memory for the vertex array
+  vertex_array = (vertex_t *)malloc(sizeof(vertex_t) * sphere.vertex_count);
+
+  // Temp variables for constructing the sphere
+  float x, y, z, xy;  // Vertex position
+  float nx, ny, nz, length_inv = 1.0f / radius;  // Vertex normal
+  float u, v; // Vertex tex coords
+  float sector_step = 2 * CGLM_PI / sectors; 
+  float stack_step = CGLM_PI / stacks;
+  float sector_angle, stack_angle;
+
+  /*
+    TODO: Finish Sphere construction here
+  */
+
+  sphere.draw_mode = GL_TRIANGLES;  // Set draw mode to triangles
+
+  return sphere;  // Return the sphere created
 }
 
 void freeMesh(mesh_t *mesh) {
